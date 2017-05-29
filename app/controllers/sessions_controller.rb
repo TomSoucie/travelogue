f5a2d4c85e0b76c6b09e1f41df6633764a1858d1
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-29 13:13:01 -0600

    Commit current_user method and passing user_can_login_spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # byebug
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully logged in"
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
-
 end
