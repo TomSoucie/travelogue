c1c2419bc70be5bf39d1163f87a0c57805e55ad6
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-13 16:54:35 -0600

    add spec outline to user_can_view_their_ideas_spec

---
+++ spec/features/ideas/user_can_view_all_their_ideas_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "user can view all their ideas" do
+  let!(:idea_1) {create(:idea)}
+  let!(:idea_2) {create(:idea)}
+  let!(:idea_3) {create(:idea)}
+
+  visit ideas_path
+
+  expect(page).to have_content(idea_1.title)
+  expect(page).to have_content(idea_2.title)
+  expect(page).to have_content(idea_3.title)
+end
