ac24ea4bad31233435bdf5b0e6523f86877289c6
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-14 15:45:09 -0600

    create ideas_controller

--- spec/features/ideas/user_can_create_idea_spec.rb
+++ spec/features/ideas/user_can_create_idea_spec.rb
@@ -1,25 +1,27 @@
 require 'rails_helper'
 
 RSpec.feature "user can create a new idea" do
-  scenario "user logs in and can create a new idea" do
+  before do
+    @user = create(:user)
+    @categories = create_list(:category, 3)
 
-    visit login_path
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
 
-    within("form") do
-      fill_in "Email", with: registered.email
-      fill_in "Password", with: registered.password
-      click_on "Login"
-    end
+  scenario "user creates idea" do
 
-    # expect(current_path).to eq('/ideas')
+    visit new_idea_path
+
+    expect(current_path).to eq('/ideas/new')
+
     click_on "New Idea"
 
     within ("form") do
       fill_in "Title", with: "Daycation to Morrision"
       fill_in "Description", with: "Take the SUPs up to Soda Lakes and have Brunch in Morrison"
-      fill_in "Category", with: "Day Trip"
-      # fill_in "Photo", with: "photo_selection"
-      click_on "Submit"
+      find_by_id("idea_category_ids#{@categories[1].id}").set(true)
+      fill_in "url", with: "https://c1.staticflickr.com/9/8582/28567087656_ad5401240e_k.jpg"
+      click_on "Create"
     end
 
     expect(page).to have_current_path("users/#{user.id}")
