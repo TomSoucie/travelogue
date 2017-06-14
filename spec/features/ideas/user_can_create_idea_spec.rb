d0f75ef9beb380773c1467a0e7c62aab6fb87599
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-14 16:37:23 -0600

    create ideas/new view to create a new idea

--- spec/features/ideas/user_can_create_idea_spec.rb
+++ spec/features/ideas/user_can_create_idea_spec.rb
@@ -14,12 +14,10 @@ RSpec.feature "user can create a new idea" do
 
     expect(current_path).to eq('/ideas/new')
 
-    click_on "New Idea"
-
     within ("form") do
       fill_in "Title", with: "Daycation to Morrision"
       fill_in "Description", with: "Take the SUPs up to Soda Lakes and have Brunch in Morrison"
-      find_by_id("idea_category_ids#{@categories[1].id}").set(true)
+      select 'Trip1', from: @categories
       fill_in "url", with: "https://c1.staticflickr.com/9/8582/28567087656_ad5401240e_k.jpg"
       click_on "Create"
     end
